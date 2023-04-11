eb9bfed6847516399b834e2056c15252e0b80b8a
Kuesung Park <gueit214@naver.com>
2023-04-12 07:34:14 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,42 @@
+
+def sol(n,arr,total_lst):
+    for i in range(n):
+        num = arr[i]
+        a=n-1
+        lst=list()
+        while True:
+            if num==0:
+                break
+            if 2**a>num:
+                a-=1
+            else:
+                lst.append(a)
+                num-=2**a
+        for j in lst:
+            total_lst[i][n-1-j]=1
+
+def solution(n, arr1, arr2):
+    total_lst = [[0 for col in range(n)] for row in range(n)]
+
+    sol(n,arr1,total_lst)
+    sol(n,arr2,total_lst)
+    
+    prt_lst=list()   
+
+    for i in total_lst:
+        str=''
+        for j in i:
+            if(j==1) :
+                str+='#'
+            else:
+                str+=' '
+        prt_lst.append(str)
+    
+    return prt_lst
+
+
+
+
+
+
+
