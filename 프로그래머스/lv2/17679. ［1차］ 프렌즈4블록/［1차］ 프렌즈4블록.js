8e2b2b128516bb34fe5aceee8a71fb6fb792bb2c
Kuesung Park <gueit214@naver.com>
2023-04-21 08:30:14 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 115.30 ms, Memory: 51.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -0,0 +1,68 @@
+function solution(m, n, board) {
+    var answer = 0;
+    
+    const check = (y,x) => {
+        let tmp = board[y][x];
+        for(let i=0;i<2;i++){
+            for(let j=0;j<2;j++){
+                if(board[y+i][x+j]!==tmp){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    let arr = []
+    board = board.map(it=>it.split(''))
+    
+    let toggle2;
+    const func = () => {
+        toggle2 = false;
+        for(let i=0;i<m-1;i++){
+            for(let j=0;j<n-1;j++){
+                if(check(i,j)){
+                    toggle2 = true;
+                    for(let a=0;a<2;a++){
+                        for(let b=0;b<2;b++){
+                            arr.push([i+a,j+b]);                            
+                        }
+                    }
+
+                }
+            }
+        }
+        arr.forEach(it=>{
+            board[it[0]][it[1]]=null
+        })
+        let toggle = false;
+        do{
+            toggle = false;
+            board.forEach((item,index)=>{
+               item.forEach((it,i)=>{
+                   if(it===null && index>0 && board[index-1][i]!==null){
+                       toggle = true;
+                       let tmp = board[index-1][i];
+                       board[index-1][i]=null;
+                       board[index][i]=tmp
+                   }
+               }) 
+            })
+        }while(toggle)
+        arr=[];
+    }
+    for(let i=0;i<1000;i++){
+    func();        
+    }
+    
+    
+        
+    console.log(arr,board)
+    let cnt = 0;
+    board.forEach(item=>{
+        item.forEach(it=>{
+            if(it===null) cnt++;
+        })
+    })
+    
+    return cnt;
+}
