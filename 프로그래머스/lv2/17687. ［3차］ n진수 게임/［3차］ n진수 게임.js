01b829c6c7657960bd48eaf7ce5a44a371add8cd
Kuesung Park <gueit214@naver.com>
2023-05-14 08:38:34 +0900

    [level 2] Title: [3차] n진수 게임, Time: 3.64 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -0,0 +1,19 @@
+function solution(n, t, m, p) {
+    var answer = '';
+//     문자열에 더해가는 방식
+    let word = "";
+    for(let i=0;;i++){   
+        if(word.length>t*m) break;
+        word+=i.toString(n).toUpperCase();
+    }
+    
+//     인원(m)은 사이클을 결정, 순서(p)는 순서를 결정
+    for(let i=0;i<word.length;i++){
+        // console.log(word[i])
+        if((i+1)%m===p%m) answer+=word[i]
+    }
+    // console.log(word)
+    
+    
+    return answer.slice(0,t);
+}
