8a4d88a977582ec14b67b97fc3e00327ed867870
Kuesung Park <gueit214@naver.com>
2023-05-14 11:17:03 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.54 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.js
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.js
@@ -1,48 +1,53 @@
-const iskeepingDistance = (place) => {
-  let roomInfo = place.map((rooms) => rooms.split(''));
-
-  let queue = [];
-  for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
-      if (roomInfo[i][j] === 'P') {
-        queue.push([i, j]);
-      }
-    }
-  }
-
-  let dx = [-1, 1, 0, 0];
-  let dy = [0, 0, 1, -1];
-
-  while (queue.length) {
-    const [x, y] = queue.shift();
-
-    for (let i = 0; i < 4; i++) {
-      let nx = x + dx[i];
-      let ny = y + dy[i];
-
-      if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
-      if (roomInfo[nx][ny] === 'X') continue;
-      if (roomInfo[nx][ny] === 'P') return 0;
-
-      for (let j = 0; j < 4; j++) {
-        let aroundNX = nx + dx[j];
-        let aroundNY = ny + dy[j];
-
-        if (aroundNX < 0 || aroundNX >= 5 || aroundNY < 0 || aroundNY >= 5) continue;
-        if (aroundNX === x && aroundNY === y) continue;
-        if (roomInfo[aroundNX][aroundNY] === 'P') return 0;
-      }
-    }
-  }
-
-  return 1;
-};
-
 function solution(places) {
-  let keepingDistance = [];
-  for (let i = 0; i < 5; i++) {
-    keepingDistance.push(iskeepingDistance(places[i]));
-  }
+    const result = []
+    const manhatenA = [[2,0],[-2,0],[0,2],[0,-2]]
+    const manhatenB = [[1,1],[1,-1],[-1,1],[-1,-1]]
+    const manhatenC = [[1,0],[-1,0],[0,1],[0,-1]]
+    
+    const isGood = (arr,y,x) => {
+        for(let i=0;i<4;i++){ // P가 위치에 있다면 무조건 false
+            let item = manhatenC[i];
+            const [goY,goX] = [y+item[0],x+item[1]];
+            if(goY>=5 || goX>=5 || goY<0 || goX<0) continue;
+            
+            if(arr[goY][goX]==='P') return false;
+        }
+        for(let i=0;i<4;i++){ // 사이에 O가 있다면 무조건 false. P라면 manhatenC에서 이미 걸러짐
+            let item = manhatenA[i];
+            const [goY,goX] = [y+item[0],x+item[1]];
+            if(goY>=5 || goX>=5 || goY<0 || goX<0) continue;
+            
+            const [middleY,middleX] = [y+item[0]/2,x+item[1]/2]
+            
+            if(arr[goY][goX]==='P' && arr[middleY][middleX]==='O') return false;
+        }
+        for(let i=0;i<4;i++){ // 칸막이가 둘 사이에 2개 있어야 함
+            let item = manhatenB[i];
+            const [goY,goX] = [y+item[0],x+item[1]];
+            if(goY>=5 || goX>=5 || goY<0 || goX<0) continue;
+            
+            if(arr[goY][goX]==='P'){
+                let [middleY,middleX] = [y,x+item[1]]
+                if(arr[middleY][middleX]==='O') return false;
 
-  return keepingDistance;
+                [middleY,middleX] = [y+item[0],x]
+                if(arr[middleY][middleX]==='O') return false;
+            }
+        }
+        return true;   
+        
+    }
+    
+    places.forEach((place)=>{
+        let isGoodBoolean = true;
+        place.forEach((column,y)=>{
+            column.split('').forEach((person,x)=>{
+                if(person==='P') {
+                    if(!isGood(place,y,x)) isGoodBoolean=false;
+                }
+            })
+        })
+        result.push(isGoodBoolean?1:0)
+    })    
+    return result;
 }
