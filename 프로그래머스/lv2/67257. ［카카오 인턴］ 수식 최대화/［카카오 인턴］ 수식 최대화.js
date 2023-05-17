d1c6611201be9744a8dfc7a223b07d0ece29e2f6
Kuesung Park <gueit214@naver.com>
2023-05-17 17:08:56 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.48 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.js
@@ -0,0 +1,48 @@
+function solution(expression) {
+    var answer = 0;
+    
+//     6가지 경우의 수
+//     절대값이 가장 큰 경우는?
+    
+    const calculateIt = (a,b,expression) => {
+        [a,b] = [+a,+b]
+        if(expression==='*') return a*b;
+        if(expression==='+') return a+b;
+        if(expression==='-') return a-b;
+    }
+    
+    const calculateEach = (numArr,exArr,calculateNow) => {
+        let thisPos = exArr.indexOf(calculateNow);
+        while(thisPos!==-1){
+            const result = calculateIt(numArr[thisPos],numArr[thisPos+1],calculateNow)
+            exArr.splice(thisPos,1);
+            numArr.splice(thisPos,2,result);
+            thisPos = exArr.indexOf(calculateNow);
+        }
+        return numArr[0]
+    }
+    
+    const express = ['+','-','*']
+    const calculate = (numArr,exArr, item) => {
+        item.forEach(it=>{
+            expression = calculateEach(numArr,exArr, express[it])
+        })
+        return expression
+    }
+    
+    const cases = [
+        [0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,0,1],[2,1,0]
+    ]
+    
+    const numArr = expression.split(/[*+-]/g);
+    const exArr = expression.split('').filter(it=>isNaN(it))
+    
+    cases.forEach(item=>{
+        const numArrDup = [...numArr];
+        const exArrDup = [...exArr]
+        const result = calculate(numArrDup,exArrDup,item)
+        answer = Math.max(answer,result>0?result:(-1)*result)
+    })
+    
+    return answer;
+}
