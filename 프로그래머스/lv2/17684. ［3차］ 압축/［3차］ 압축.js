98b9afea9fbdb64a2033dafb6627e64dfc7e42c5
Kuesung Park <gueit214@naver.com>
2023-05-21 00:53:09 +0900

    [level 2] Title: [3차] 압축, Time: 10.51 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -0,0 +1,28 @@
+const solution = (msg) => {
+    const answer = []
+    // 문자열 저장 객체
+    const wordObject = {};
+  	// 초기화 작업
+    for (let i=0; i<26; i++) {
+        const word = String.fromCharCode(65+i);
+        wordObject[word] = i+1;
+    };
+  
+    
+    let idx = 0;
+    while (idx < msg.length) {
+        let nowWord = '';
+      	// 새로운 문자열이 나타나지 않을 때까지 문자열 붙히기
+        while (wordObject[nowWord + msg[idx]]) {
+            nowWord += msg[idx]
+            idx += 1;
+            console.log(nowWord)
+        }
+        answer.push(wordObject[nowWord]);
+      	// 새롭게 발견된 문자는 객체 길이의 +1 값이 value로 들어간다.
+        const N = Object.keys(wordObject).length+1;	
+      	// 새롭게 추가될 문자 저장
+        wordObject[nowWord+msg[idx]] = N;
+    }
+    return answer
+}
