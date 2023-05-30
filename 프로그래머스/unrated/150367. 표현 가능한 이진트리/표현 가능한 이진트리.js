60b4d032972f8070fa80a7e7e3719d3647ff9d6e
Kuesung Park <gueit214@naver.com>
2023-05-31 08:38:52 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 23.75 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.js
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.js
@@ -1,42 +1,42 @@
 function solution(numbers) {
-    const answer = [];
-    for (let number of numbers) {
-        //이진수 변환
-        let bi_num = number.toString(2);
-        let n = bi_num.length;
-        //이진트리를 만들기 위한 노드 개수
-        let m = n.toString(2).length
-        // 이진수 앞에 0 추가
-        let bi_tree = '0'.repeat(2**m-1 - n);
-        bi_tree = bi_tree + '' + bi_num;
-        if (checkBTree(bi_tree, 0, bi_tree.length-1)) {
-            answer.push(1);
-        }
-        else {
-            answer.push(0);
-        }
-    }
-    return answer;
-}
+//     1. 2진수로 변환
+//     2. 1,3,7,... 문자열에 맞춰서 앞에 0을 붙힘
+//     3. 각 경우 별 트리의 조건에 성립하는 지 판단 -> 트리인지 체크하는 함수 (재귀함수)
 
-function checkBTree (b_str, start, end) {
-    //부모 노드 idx
-    const mid = Math.floor((start+end) / 2);
-    //자식 노드 idx
-    const left_c = Math.floor((start + mid-1)/2);
-    const right_c = Math.floor((mid+1 + end)/2);
+    const makeTree = (number) => {
+        let [num,temp] = [0,1];
+        while(num<number.length){
+            num+=temp;
+            temp*=2;
+        }
+        number = '0'.repeat(num-number.length)+ number ;
+        return [number, 0, number.length-1]
+    }
     
-    //리프노드 도달
-    if (start == end) {
+    
+    function isTree ([b_str, start, end]) {
+        //부모 노드 idx
+        const mid = Math.floor((start+end) / 2);
+        //자식 노드 idx
+        const left_c = Math.floor((start + mid-1)/2);
+        const right_c = Math.floor((mid+1 + end)/2);
+
+        //리프노드 도달
+        if (start == end) {
+            return true;
+        }
+
+        
+        //부모가 0 인데 자식에 1이 있으면 안됨 -> 이 경우 false 를 반환
+        if (b_str[mid] === '0' && ((b_str[left_c] === '1') || (b_str[right_c] === '1'))) {
+            return false;
+        }
+
+        if (!isTree([b_str, start, mid-1])) return false;
+        if (!isTree([b_str, mid+1, end])) return false;
         return true;
     }
-    
-    //부모가 0 인데 자식에 1이 있으면 안됨 -> 이 경우 false 를 반환
-    if (b_str[mid] === '0' && ((b_str[left_c] === '1') || (b_str[right_c] === '1'))) {
-        return false;
-    }
 
-    if (!checkBTree(b_str, start, mid-1)) return false;
-    if (!checkBTree(b_str, mid+1, end)) return false;
-    return true;
+    return numbers.map(v=>isTree(makeTree(v.toString(2)))?1:0)
+
 }
