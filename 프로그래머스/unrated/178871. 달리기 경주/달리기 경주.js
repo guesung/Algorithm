f6a1dff43c2a6a10da7c17b82a4572171b8265d4
Kuesung Park <gueit214@naver.com>
2023-09-17 14:27:01 +0900

    [unrated] Title: 달리기 경주, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,25 +1,22 @@
 function solution(players, callings) {
-    const playerIndices = {}; // 이름을 키로, 인덱스를 값으로 가지는 객체
-
-    players.forEach((player, index) => {
-        playerIndices[player] = index;
-    });
-
-    callings.forEach(calling => {
-        const playerIndex = playerIndices[calling];
-        if (playerIndex > 0) { // 첫번째 원소가 아니라면 스왑 수행
-            const playerFrontIndex = playerIndex - 1;
-            const playerFrontName = players[playerFrontIndex];
-
-            // 인덱스 업데이트
-            playerIndices[calling] = playerFrontIndex;
-            playerIndices[playerFrontName] = playerIndex;
-
-            // players 배열의 원소 스왑
-            players[playerIndex] = playerFrontName;
-            players[playerFrontIndex] = calling;
-        }
-    });
+    const obj = {};
+    
+    players.forEach((player,index)=>{
+        obj[player] = index;
+    })
+    
+    callings.forEach((calling)=>{
+        const playerName = calling;
+        const playerIndex = obj[calling]
+        const playerFrontIndex = playerIndex-1;
+        const playerFrontName = players[playerFrontIndex]
+        
+        players[playerIndex] = playerFrontName
+        players[playerFrontIndex] = playerName
+        
+        obj[playerName] = playerFrontIndex;
+        obj[playerFrontName] = playerIndex;
+    })
 
     return players;
 }
