1b37cf7e677fc56ee514a7bfa96a7737f85f1789
Kuesung Park <gueit214@naver.com>
2023-09-22 13:55:33 +0900

    [unrated] Title: 과일 장수, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,11 +1,6 @@
 function solution(k, m, score) {
     var answer = 0;
-//     박스 수 = score.length/m;
-//     가장 큰 숫자끼리 모아놓는 것이 이득
-//     정렬
-    
     const sortedScore = score.sort((a,b)=>b-a)
-//     앞에서부터 m개씩 곱하고 이를 더함
     let arr = [];
     let sum = 0;
 
