69e596bdc2d0f481870ff6de523df0ebd29404cd
Kuesung Park <gueit214@naver.com>
2023-12-26 14:22:40 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 50.90 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -1,63 +1,52 @@
 function solution(land) {
-  const n = land.length;
-  const m = land[0].length;
-  const moves = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-  const dp = new Array(m + 1).fill(0);
+  const dR = [-1, 0, 1, 0];
+  const dC = [0, 1, 0, -1];
+  const rLen = land.length;
+  const cLen = land[0].length;
+  const oilByCol = {}; // {열: 뽑을 수 있는 석유량}
 
-  const bfs = (moveGroup, landObj) => {
-    const newMoveGroup = [];
-    landObj.area += moveGroup.length;
+  for (let c = 0; c < cLen; ++c) {
+    const queue = [];
+    // 한 열에서 row를 순회하며 석유 찾기
+    for (let r = 0; r < rLen; ++r) {
+      // 석유를 찾음. 연결된 총 석유량 구해서 oilByCol에 더하기
+      if (land[r][c] === 1) {
+        land[r][c] = 0; // 현재 위치는 이미 뽑은 석유로 변경
+        let sum = 1; // 석유량 초기화
+        let connectedCols = new Set(); // 현재위치와 연결된 열의 set
+        connectedCols.add(c);
+        queue.push({ r, c });
 
-    for (const [i, j] of moveGroup) {
-      if (landObj.maxJ < j) landObj.maxJ = j;
-      if (landObj.minJ > j) landObj.minJ = j;
+        // 현재위치와 연결된 열과 총 석유량 구하기
+        while (queue.length) {
+          const { r, c } = queue.shift();
+          for (let i = 0; i < 4; ++i) {
+            const nextR = r + dR[i];
+            const nextC = c + dC[i];
 
-      for (const [mi, mj] of moves) {
-        const [movedI, movedJ] = [mi + i, mj + j];
-        if (
-          movedI < n &&
-          movedI >= 0 &&
-          movedJ < m &&
-          movedJ >= 0 &&
-          land[movedI][movedJ]
-        ) {
-          land[movedI][movedJ] = 0;
-          newMoveGroup.push([movedI, movedJ]);
+            if (
+              nextR >= 0 &&
+              nextR < rLen &&
+              nextC >= 0 &&
+              nextC < cLen &&
+              land[nextR][nextC] === 1
+            ) {
+              // 주변에 석유가 있고 새로운 열이면 연결된 열set에 추가
+              if (!connectedCols.has(nextC)) connectedCols.add(nextC);
+              sum++;
+              land[nextR][nextC] = 0;
+              queue.push({ r: nextR, c: nextC }); // 주변열의 다음 주변을 확인한다.
+            }
+          }
         }
-      }
-    }
-
-    if (newMoveGroup.length) bfs(newMoveGroup, landObj);
-  };
-
-  for (let i = 0; i < n; i += 1) {
-    for (let j = 0; j < m; j += 1) {
-      if (land[i][j]) {
-        const landObj = { area: 0, minJ: j, maxJ: j };
-        land[i][j] = 0;
-        bfs([[i, j]], landObj);
-
-        dp[landObj.minJ] += landObj.area;
-        dp[landObj.maxJ + 1] -= landObj.area;
+        // oilByCol의 연결된 열에 현재 석유그룹의 총 석유량 더하기
+        for (const c of connectedCols) {
+          oilByCol[c] = (oilByCol[c] || 0) + sum;
+        }
+        connectedCols = new Set(); // 다음 석유그룹을 찾기위해 초기화
       }
     }
   }
-
-  let sum = 0;
-  let result = 0;
-
-  for (let i = 0; i < m; i += 1) {
-    if (dp[i]) {
-      sum += dp[i];
-
-      if (sum > result) result = sum;
-    }
-  }
-
-  return result;
+  const values = Array.from(Object.values(oilByCol));
+  return Math.max(...values);
 }
