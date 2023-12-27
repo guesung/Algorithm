fafeb7ff696d6f7b01efbf78ae68e9ed89426253
Kuesung Park <gueit214@naver.com>
2023-12-27 12:24:30 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 27.45 ms, Memory: 51.2 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -1,52 +1,47 @@
 function solution(land) {
-  const dR = [-1, 0, 1, 0];
-  const dC = [0, 1, 0, -1];
-  const rLen = land.length;
-  const cLen = land[0].length;
-  const oilByCol = {}; // {열: 뽑을 수 있는 석유량}
-
-  for (let c = 0; c < cLen; ++c) {
-    const queue = [];
-    // 한 열에서 row를 순회하며 석유 찾기
-    for (let r = 0; r < rLen; ++r) {
-      // 석유를 찾음. 연결된 총 석유량 구해서 oilByCol에 더하기
-      if (land[r][c] === 1) {
-        land[r][c] = 0; // 현재 위치는 이미 뽑은 석유로 변경
-        let sum = 1; // 석유량 초기화
-        let connectedCols = new Set(); // 현재위치와 연결된 열의 set
-        connectedCols.add(c);
-        queue.push({ r, c });
-
-        // 현재위치와 연결된 열과 총 석유량 구하기
-        while (queue.length) {
-          const { r, c } = queue.shift();
-          for (let i = 0; i < 4; ++i) {
-            const nextR = r + dR[i];
-            const nextC = c + dC[i];
-
-            if (
-              nextR >= 0 &&
-              nextR < rLen &&
-              nextC >= 0 &&
-              nextC < cLen &&
-              land[nextR][nextC] === 1
-            ) {
-              // 주변에 석유가 있고 새로운 열이면 연결된 열set에 추가
-              if (!connectedCols.has(nextC)) connectedCols.add(nextC);
-              sum++;
-              land[nextR][nextC] = 0;
-              queue.push({ r: nextR, c: nextC }); // 주변열의 다음 주변을 확인한다.
+//     bfs문으로 주변 석유를 탐색
+//     n은 세로 길이(행의 개수), m은 가로 길이(열의 개수)
+    const [n,m] = [land.length,land[0].length];
+    const directionList = [[-1,0],[1,0],[0,-1],[0,1]];
+    
+    const bfs = (landArray,landObj) => {
+        landObj.area += landArray.length;
+        const newLandArray = [];
+        
+        for(const [y,x] of landArray) {        
+            landObj.minX = Math.min(landObj.minX,x);
+            landObj.maxX = Math.max(landObj.maxX,x);
+            
+            for(const direction of directionList){
+                const [directionY,directionX] = direction;
+                const [moveY,moveX] = [y+directionY,x+directionX];
+                if(moveY<n && moveY>=0 && moveX<m && moveX>=0 && land[moveY][moveX]) {
+                    newLandArray.push([moveY,moveX])
+                    land[moveY][moveX] = 0;
+                }
             }
-          }
         }
-        // oilByCol의 연결된 열에 현재 석유그룹의 총 석유량 더하기
-        for (const c of connectedCols) {
-          oilByCol[c] = (oilByCol[c] || 0) + sum;
-        }
-        connectedCols = new Set(); // 다음 석유그룹을 찾기위해 초기화
-      }
+        if(newLandArray.length) bfs(newLandArray,landObj)
     }
-  }
-  const values = Array.from(Object.values(oilByCol));
-  return Math.max(...values);
+    const dp = new Array(m+1).fill(0);
+//     각 석유 덩어리의 시작점과 끝점, 그리고 석유 크기를 알면 된다.
+    for(let i=0;i<n;i++){
+        for(let j=0;j<m;j++){
+            const landObj = {area:0,minX:j,maxX:j}
+            if(land[i][j]) {
+                land[i][j] = 0
+                bfs([[i,j]],landObj)
+                
+                dp[landObj.minX] += landObj.area;
+                dp[landObj.maxX + 1] -= landObj.area;
+            }
+        }
+    }
+    let sum = 0;
+    let result = 0;
+    for(let i=0;i<m;i++){
+        sum += dp[i]
+        result = Math.max(sum,result);
+    }
+    return result;
 }
