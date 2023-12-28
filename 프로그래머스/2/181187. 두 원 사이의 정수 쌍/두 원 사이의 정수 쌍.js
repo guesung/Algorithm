eab9a9e26e9c771ef4271815d508bc8ce2c2b01d
Kuesung Park <gueit214@naver.com>
2023-12-28 11:50:41 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 9.80 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
@@ -1,10 +1,26 @@
 function solution(r1, r2) {
-    let diagonalSum = 0;
-    for(let x=1;x<r2;x++){
-        const minValue = x<r1 ? Math.ceil(Math.sqrt(r1 * r1 - x * x)) : 1
-        const maxValue = Math.floor(Math.sqrt(Math.abs(r2 * r2 - x * x))); 
-        diagonalSum += (maxValue - minValue + 1)
+    var answer = 0;
+//     12 + 8 = 20
+//     핵심 : 한 분면을 구해서, *4하면 된다.
+//     x혹은 y축에 있는 점은 뺄셈으로 쉽게 구할 수 있다.
+//     for문 : 1부터 r2-1 까지 순회
+//     각 for문에서 r1**2 < i**2 + y**2 < r2**2 를 만족하는 y의 개수 구하기
+//     r1**2 - i**2 < y**2 < r2**2 - i**2 
+//     만약 r2가 100만이라면, 시간복잡도는 100만 ** 2이 될 것이다.
+//     아닌가, 그 사이에 있는 점의 개수만 구하면 되기 때문에 시간복잡도가 100만일 수 있다.
+//     사이에 몇 개 있는지 어떻게 알지. 
+
+    let sum = r2 - r1 + 1;
+    for(let i=1;i<r2;i++){
+        const minX = Math.ceil((r1**2 - i**2)**(1/2)) || 1;
+        const maxX = Math.floor((r2**2 - i**2)**(1/2)) || 1;
+        
+        sum += (maxX - minX + 1)
     }
-    const axiosSum = r2 - r1 + 1
-    return (diagonalSum + axiosSum)*4;
+    
+    
+    
+    
+    
+    return sum*4;
 }
