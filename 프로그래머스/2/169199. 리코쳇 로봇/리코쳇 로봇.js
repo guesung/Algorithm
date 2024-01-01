7aba2f577b69f1362f96d000f8b4a512902f9757
Kuesung Park <gueit214@naver.com>
2024-01-01 12:09:13 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.61 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -1,45 +1,62 @@
 function solution(board) {
-    const visited = Array.from(Array(board.length),()=>Array(board[0].length).fill(false))
     var answer = 0;
-    const queue = [];
-    let start,end;
-    const gX=[0,0,1,-1];
-    const gY=[1,-1,0,0];
+    board = board.map(items => items.split(''));
     
+    const q = [];
+    const n =board.length;     // 가로 길이
+    const m = board[0].length; // 세로 길이
+    const dx = [-1, 1, 0, 0];  // 상하좌우 방향
+    const dy = [0, 0, -1, 1];
     
-    board.forEach((line,index)=>{
-        if(line.indexOf('R')!==-1){
-            start = [index,line.indexOf('R')]
-        }
-        if(line.indexOf('G')!==-1){
-            end = [index,line.indexOf('G')]
-        }
-    })  
+    board.forEach((items, i) => {
+        items.forEach((item, j) => {
+            if(item === 'R') q.push([i, j]);  // 시작 위치
+        });
+    });
+	
+    // 1) 시작 위치를 다시 방문하지 않게 'O' 표시
+    board[q[0][0]][q[0][1]] = 'O';   
     
-    queue.push([start[0],start[1],0]);
-    while(queue.length>0){
+    // 2) q의 길이 만큼 반복
+    while(q.length) {
+    
+        // 3) 횟수(answer)를 카운트하기 위해 현재 q의 길이를 고정시킨다.
+        const size = q.length;
         
-        let now = queue.shift();
-        if(visited[now[0]][now[1]]) continue;
-        visited[now[0]][now[1]]=true;
-        if(now[0]===end[0] && now[1]===end[1]) {
-            return now[2]
-        }
-        
-        for(let i=0;i<4;i++){
-            let [y,x,cnt]=now;
-            while(true){
-                x+=gX[i]
-                y+=gY[i]
-                if(y<0 || x<0 || y>=board.length || x>=board[0].length || board[y][x]==='D') break;
+        // 4) 고정시킨 길이만큼 반복한다.
+        for(let i=0; i<size; i++) {
+            const [x, y] = q.shift();
+          
+            // 5) 상하좌우 한번씩 확인
+            for(let j=0; j<4; j++) {
+            
+                // 6) 다음 이동 위치
+                let nx = x + dx[j];
+                let ny = y + dy[j];
+                
+                // 7) 게임판 범위와 벽(D)를 만나지 않을 경우만 미끄러진다.
+                while(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] !== 'D') {
+                    nx += dx[j];
+                    ny += dy[j];
+                }
+                
+                // 8) 현재 위치로 변경한다.
+                nx -= dx[j];
+                ny -= dy[j];
+                
+                // 9) 현재 위치가 도착(G) 지점이면 횟수(answer)를 1증가 후 반환한다. 
+                if(board[nx][ny] === 'G') return answer+1;
+                
+                // 10) 한번이라도 방문한적이 없을 경우만
+                if(board[nx][ny] !== 'O') {
+                
+                    // 11) 방문 표시(O) 후 q에 담는다.
+                    board[nx][ny] = 'O';
+                    q.push([nx, ny]);
+                }
             }
-            x-=gX[i]
-            y-=gY[i]
-            if(y===now[0] && x===now[1]) continue;
-            else {
-                queue.push([y,x,cnt+1])
-            }
         }
+        answer++;
     }
     
     return -1;
