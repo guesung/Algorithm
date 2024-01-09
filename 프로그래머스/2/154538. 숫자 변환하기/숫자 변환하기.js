abce203cb0a4cfe9d8ce17ab15c2becfaf5abbca
Kuesung Park <gueit214@naver.com>
2024-01-09 11:57:50 +0900

    [level 2] Title: 숫자 변환하기, Time: 28.00 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,11 +1,16 @@
 function solution(x, y, n) {
     var answer = 0;
-    const dp = new Array(y+1).fill(Infinity);
+    
+//     DP
+//     x+1부터 y까지의 값에, 변환 횟수를 업데이트 해나가는 방식
+//     큰 수부터 순회해야 더 효율적으로 순회할 수 있다.
+    const dp = new Array(y+1).fill(Infinity); // dp는 각 값이 되기 위한 실행 횟수
     dp[x] = 0;
     for(let i=x+1;i<=y;i++){
-        if(i-n>=x) dp[i] = Math.min(dp[i],dp[i-n]+1);
-        if(i%2===0 && i/2>=x) dp[i] = Math.min(dp[i],dp[i/2]+1);
-        if(i%3===0 && i/3>=x) dp[i] = Math.min(dp[i],dp[i/3]+1);
+        if(i-n >= x) dp[i] = Math.min(dp[i],dp[i-n]+1);
+        if(i%2===0 && i/2 >= x) dp[i] = Math.min(dp[i],dp[i/2]+1);
+        if(i%3===0 && i/3 >= x) dp[i] = Math.min(dp[i],dp[i/3]+1);
     }
-    return dp[y]===Infinity?-1:dp[y];
+    
+    return dp[y] === Infinity ? -1 : dp[y];
 }
