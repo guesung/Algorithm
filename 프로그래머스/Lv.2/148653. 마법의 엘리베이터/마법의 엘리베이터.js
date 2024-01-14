33bbe22ff1caa89d7cbb84fc008c8a9fd3fc7c54
Kuesung Park <gueit214@naver.com>
2024-01-14 12:40:46 +0900

    [level Lv.2] Title: 마법의 엘리베이터, Time: 4.53 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/Lv.2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/Lv.2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,33 +1,14 @@
 function solution(storey) {
-//     위로 갔다가 내려갈 수도 있고, 그냥 내려갈 수도 있다. 
-//     storey에서 시작해서 0으로 가야한다.
-    let answer = Infinity;
-    const dfs = (number,cnt) => {
-        console.log(number,cnt)
-        if(number < 10){
-            switch(number){
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                    cnt += number;
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                    cnt += (11-number);
-                    break;
-            }
-            answer = Math.min(answer,cnt);
-            return;
-        }
-        const afterNumber = Math.floor(number/10);
-        dfs(afterNumber,cnt + number%10)
-        dfs(afterNumber + 1,10-number%10+cnt);
+  let answer = Number.MAX_SAFE_INTEGER;
+  function dfs(num, counter) {
+    if (counter >= answer) return;
+    if (num === 0) answer = counter;
+    else {
+      let res = num % 10;
+      dfs(Math.floor(num / 10), counter + res);
+      dfs(Math.floor(num / 10) + 1, counter + 10 - res);
     }
-    dfs(storey,0)
-    
-    return answer;
+  }
+  dfs(storey, 0);
+  return answer;
 }
