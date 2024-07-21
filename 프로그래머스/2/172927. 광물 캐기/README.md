ad982db85a81cf065d05afaadcf45c93d5abfe26
Kuesung Park <gueit214@naver.com>
2024-07-21 11:34:10 +0900

    [level 2] Title: 광물 캐기, Time: 0.48 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927#) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 3.20 ms
+메모리: 33.5 MB, 시간: 0.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 5일 13:17:32
+2024년 07월 21일 11:34:04
 
 ### 문제 설명
 
--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,66 +1,73 @@
-const PICK_LIST = ['diamond', 'iron', 'stone'];
-const getCountOfItem = (array,item) => array.filter(it => it === item).length
-const MINERAL_RATIOS = {
-  'diamond': {'diamond': 1, 'iron' : 1, 'stone' : 1},
-  'iron': {'diamond': 5, 'iron': 1, 'stone': 1},
-  'stone': {'diamond': 25, 'iron': 5, 'stone': 1}
-};
-const calculatePickMineral = (pick, mineral, count) => {
-  return MINERAL_RATIOS[pick][mineral] * count;
-}
-
 function solution(picks, minerals) {
     var answer = 0;
     
-//     현재 곡괭이로 최대로 캘 수 있는 광물 = 5 * 곡괭이 수
-    const maxMineralCount = picks.reduce((prev,cur) => prev + cur,0) * 5
-    const validMinerals = minerals.slice(0,maxMineralCount)
-
-//     0. 5개씩 묶기 (한 번 곡괘이를 잡으면 5개씩 캐야 함)
-    const dividedMinerals = [];
-    for(let i=0;i<validMinerals.length;i+=5){
-        dividedMinerals.push(validMinerals.slice(i,i+5))
+//     다이아, 철, 돌 각각 0~5개
+    
+//     pick : [dia,iron,stone] | minerals : 광물의 순서
+//     1. 한 곡괭이를 들었으면 5개를 꼭 캐야 한다.
+//     2. 그 후, 새로운 곡괭이로 교체해야 한다.
+    
+//     25 + 25 = 50
+//     만약, 모든 minerals를 캐지 못하면, 거기서 끝낸다. (예외 케이스)
+    
+    // const pickCount = picks.reduce((prev,cur)=>prev+cur,0);
+    
+//     dia -> 무조건 dia, iron -> dia>iron, stone -> stone으로 캐는 것이 이득이다.
+//     1. 5개씩 캘 것이기에, 5개씩 끊는다. 
+//     1.1. dia, iron, stone이 몇 개씩 있는 지 구한다.
+//     2. dia가 가장 많은 배열을 찾아 -> dia > iron > stone 곡괭이 순으로 있는 대로 캔다.
+//     3. iron과 stone도 동일한 과정을 반복한다.
+    function getCount(array,item){
+        return array.filter(it => it===item).length
     }
     
-//     1. diamond,iron,stone 별로 개수 세기
-    const countMinerals = dividedMinerals.map(minerals => {
-        return {
-            diamond:getCountOfItem(minerals,'diamond'),
-            iron:getCountOfItem(minerals,'iron'),
-            stone:getCountOfItem(minerals,'stone'),
-        }        
-    })
-//     2. diamond,iron,stone 순으로 정렬
-    const sortedMinerals = countMinerals.sort((a,b)=>{
-        if(a.diamond === b.diamond){
-            if(a.iron === b.iron){
-                return b.stone - a.stone
-            }
-            return b.iron - a.iron
-        }
-        return b.diamond - a.diamond
-    })
-//     3. picks에서 앞에서부터 가진 곡괭이로 캐기
-    const pickStore = [];
-    for(let i=0;i<3;i++){
-        while(picks[i]--){
-            const currentPick = PICK_LIST[i];
-            pickStore.push(currentPick)
+    function getFatigue(pick,mineral){
+        switch(pick){
+            case 'diamond':
+                return 1;
+            case 'iron':
+                if(mineral === 'diamond') return 5;
+                return 1;
+            case 'stone':
+                if(mineral === 'diamond') return 25;
+                if(mineral === 'iron') return 5;
+                return 1;
         }
     }
-    const pickStore2 = PICK_LIST.flatMap((pick, index) => {
-        if(picks[index]>0)Array(picks[index]).fill(pick)})
-                                         
-                                        
+    
+    const mineralsPerFiveArray = [];
+    const pickSum = picks.reduce((prev,cur)=>prev+cur,0);
+    const slicedMinerals = minerals.slice(0,pickSum * 5)
+    
+    while(slicedMinerals.length>0) mineralsPerFiveArray.push(slicedMinerals.splice(0,5));
+    
+    const mineralsCountArray = [];
+    for(const mineralsPerFive of mineralsPerFiveArray){
+        mineralsCountArray.push({
+            diamond:getCount(mineralsPerFive,'diamond'),
+            iron:getCount(mineralsPerFive,'iron'),
+            stone:getCount(mineralsPerFive,'stone'),
+        })
+    }
+    
+    mineralsCountArray.sort((a,b)=>b.diamond - a.diamond || b.iron - a.iron || b.stone - a.stone); // diamond, iron, stone 순으로 내림차순 정렬
+    
+    const pickArray = []; // 실제 pick 개수 (diamond, iron, stone 순)
+    const pickType = ['diamond','iron','stone']
+    picks.forEach((pick,index)=>{
+        for(let i=0;i<pick;i++){
+            pickArray.push(pickType[index]);
+        }
+    });
+    
+    const sum = mineralsCountArray.reduce((prev,curreuntMineral,index)=> {
+        const currentPick = pickArray[index];
+        if(!currentPick) return prev;
         
-
-    console.log(pickStore2)
-                                     
-    const fatigueSum = sortedMinerals.reduce((prevSum,currentMinerals,index) => {
-        const currentPick = pickStore[index];
-        const {diamond:currentDiamondCount,iron:currentIronCount,stone:currentStoneCount} = currentMinerals 
-        return prevSum + calculatePickMineral(currentPick,'diamond',currentDiamondCount) + calculatePickMineral(currentPick,'iron',currentIronCount) + calculatePickMineral(currentPick,'stone',currentStoneCount)
+        const fatigueSum = getFatigue(currentPick,'diamond') * curreuntMineral.diamond + getFatigue(currentPick,'iron') * curreuntMineral.iron + getFatigue(currentPick,'stone') * curreuntMineral.stone
+        
+        return prev + fatigueSum;
     },0)
     
-    return fatigueSum;
+    return sum;
 }
