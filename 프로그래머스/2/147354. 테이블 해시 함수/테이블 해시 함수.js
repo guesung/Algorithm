4ec9bc144e1df512a23c848229abe5a6d22ff9de
Kuesung Park <gueit214@naver.com>
2024-07-30 11:06:01 +0900

    [level 2] Title: 테이블 해시 함수, Time: 13.33 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,17 +1,27 @@
 function solution(data, col, row_begin, row_end) {
     var answer = 0;
-//     1. col을 기준으로 오름차순 정렬
-//     2. 만약 col기준으로 동일한 값이라면, 첫번째 칼럼의 값을 기준으로 내림차순 정렬
-    data.sort((a,b) => {
-        if(a[col-1]===b[col-1]) return b[0] - a[0]
-        return a[col-1] - b[col-1]
-    })
-//     3. row_begin ~ row_end인 모든 i값으로 S_i를 누적하여 더하기
-    const modArray = [];
-    for (let row=row_begin;row<=row_end;row++){
-        const currentData = data[row-1];
-        const currentSum = currentData.reduce((prev,cur)=>prev + cur % row ,0)
-        modArray.push(currentSum)
+
+//     data : 테이블의 데이터, col : 해시 함수에 대한 입력, row_begin : 
+//     열 : 컬럼, 행 : 튜플
+//     첫 번째 열 : 기본키
+//     col번째 컬럼의 값을 기준으로 오름차순 정렬. 동일하다면 기본키인 첫 번째 칼럼의 값을 기준으로 내림차순 정렬
+//     S_i : i번째 행의 튜플에 대해 각 컬럼의 값을 i로 나눈 나머지들의 합
+    
+    
+//     S_2 = (2 mod 2) + (2 mod 2) + (6 mod 2) = 0
+//     S_3 = (1 mod 3) + (5 mod 3) + (10 mod 3) = 4 
+    
+//     1. col번 째 컬럼의 값을 기준으로 올므차순 정렬
+//     2. 동일하다면 기본키인 첫 번째 칼럼을 기준으로 내림차순 정렬
+//     3. S_i : i번 째 행의 튜플에 대해 각 칼럼의 값을 i로 나눈 나머지들의 합
+    data.sort((a,b) => a[col-1] - b[col-1] || b[0] - a[0]);
+    function xor(a,b){
+        return a^b
     }
-    return modArray.reduce((prev,cur)=>prev^cur,0)
+    return data
+        .slice(row_begin-1,row_end) // 2,3
+        .reduce((prevItem,curItem,i)=>{
+            const a = curItem.reduce((prev,cur)=> prev + cur % (i+row_begin) ,0)
+            return xor(prevItem ?? 0,a);
+        },0)
 }
